8258b40ce1ad31aafcb58640c5d6443306e50a84
cjsmocjsmo <porthose.cjsmo.cjsmo@gmail.com>
2021-12-12 13:45:24 -0800

    should have persistent data now

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,13 @@
+// import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-static';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
+
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
 };
 
-//another useless comment
-//not needed comment
 export default config;
